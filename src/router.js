b5e2414501b70f02b665bbbb738b14e1cb12c9aa
Denys Sttrilchuk <denia876@gmail.com>
2024-02-15 12:34:22 +0200

    HW-Lesson 5 begin

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import {MainLayout} from "./layouts/MainLayout";
 import {ErrorPage} from "./pages/ErrorPage";
 import {UsersPage} from "./pages/UsersPage";
@@ -6,6 +6,9 @@ import {UsersPage} from "./pages/UsersPage";
 const router = createBrowserRouter([
     {
         path:'', element:<MainLayout/>, errorElement:<ErrorPage/>, children: [
+            {
+                index:true, element:<Navigate to={'users'}/>
+            },
             {
                 path:'users', element:<UsersPage/>
             }
