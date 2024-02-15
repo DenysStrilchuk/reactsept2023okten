bb37ce8736c4c20a184d79d96bea3ea4b4fe0878
Denys Sttrilchuk <denia876@gmail.com>
2024-02-15 14:04:09 +0200

    HW-Lesson 5 begin

--- src/components/UsersContainer/UsersDetails.js
+++ src/components/UsersContainer/UsersDetails.js
@@ -1,5 +1,7 @@
 import {useLocation} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {usersService} from "../../services/usersService";
+import {UserDetails} from "./UserDetails";
 
 const UsersDetails = () => {
     const {state:{id}} = useLocation();
@@ -7,11 +9,11 @@ const UsersDetails = () => {
     const [userDetails, setUsersDetails] = useState(null);
 
     useEffect(() => {
-
+        usersService.getById(id).then(({data}) => setUsersDetails(data))
     }, []);
     return (
         <div>
-            UserDetails
+            {userDetails && <UserDetails/>}
         </div>
     );
 };
