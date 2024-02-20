8d6f4683a4b61b169ec0747c063afb61357d6d6f
Denys Sttrilchuk <denia876@gmail.com>
2024-02-21 01:23:07 +0200

    lesson 6 HW 1part done do some style

--- src/components/EpisodesContainer/Episode.js
+++ src/components/EpisodesContainer/Episode.js
@@ -1,13 +1,15 @@
 import css from "../MainContainer/Main.module.css"
+import {useNavigate} from "react-router-dom";
 
 const Episode = ({episode}) => {
 const {id, name, episode:chapter} = episode;
+const navigate = useNavigate();
     return (
         <div className={css.TableText}>
 
                 <div>id:{id}</div>
                 <div>name: {name}</div>
-                <div>chapter: {chapter}</div>
+                <div onClick={() => navigate('')}>chapter: {chapter}</div>
 
 
         </div>
