ceb2d1d5c0e5401f08acf6d49965ba33ba98e71f
Denys Sttrilchuk <denia876@gmail.com>
2024-02-21 14:30:36 +0200

    lesson 6 HW done

--- src/services/characterService.js
+++ src/services/characterService.js
@@ -1,11 +1,15 @@
 import {apiService} from "./apiService";
 import {urls} from "../constants";
+import {baseURL} from "../constants/urls"
 
 const characterService = {
     getAll: () => apiService.get(urls.characters.base),
     getById: (id) => apiService.get(urls.characters.byId(id)),
+   getByEpisodeId: (episodeId) => apiService.get(urls.episodes.base)
+
+
     getByEpisodeId: async (episodeId) => {
-        const response = await apiService.get(`https://rickandmortyapi.com/api/episode/${episodeId}`);
+        const response = await apiService.get(`${baseURL}/episode/${episodeId}`);
         const characterUrls = response.data.characters;
         const characterRequests = characterUrls.map(characterUrl => apiService.get(characterUrl));
         const charactersResponses = await Promise.all(characterRequests);
