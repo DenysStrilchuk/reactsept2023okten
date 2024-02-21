de83c7206471a8904d827d61918b02899d290720
Denys Sttrilchuk <denia876@gmail.com>
2024-02-21 14:05:59 +0200

    lesson 6 HW done

--- src/components/EpisodesContainer/Episode.js
+++ src/components/EpisodesContainer/Episode.js
@@ -2,7 +2,7 @@ import css from "../MainContainer/Main.module.css"
 import {NavLink} from "react-router-dom";
 
 const Episode = ({episode}) => {
-const {id, name, episode:chapter,characters} = episode;
+const {id, name, episode:chapter} = episode;
     return (
         <div className={css.TableText}>
                 <div>id:{id}</div>
