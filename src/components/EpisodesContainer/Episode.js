970bedf50161896f67fa486b4c657479b732656d
Denys Sttrilchuk <denia876@gmail.com>
2024-02-21 12:34:50 +0200

    lesson 6 HW do style on link and make route on characters page

--- src/components/EpisodesContainer/Episode.js
+++ src/components/EpisodesContainer/Episode.js
@@ -2,12 +2,12 @@ import css from "../MainContainer/Main.module.css"
 import {NavLink} from "react-router-dom";
 
 const Episode = ({episode}) => {
-const {id, name, episode:chapter} = episode;
+const {id, name, episode:chapter,characters} = episode;
     return (
         <div className={css.TableText}>
                 <div>id:{id}</div>
                 <div>name: {name}</div>
-                <NavLink to={`/characters/${id}`}>chapter: {chapter}</NavLink>
+                <NavLink to={`/characters`}>chapter: {chapter}</NavLink>
         </div>
     );
 };
