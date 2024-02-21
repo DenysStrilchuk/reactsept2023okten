2ef2d04aab7fd7e7cdd286ee462f810aca57fe38
Denys Sttrilchuk <denia876@gmail.com>
2024-02-21 14:44:55 +0200

    lesson 6 HW done

--- src/services/characterService.js
+++ src/services/characterService.js
@@ -5,9 +5,6 @@ import {baseURL} from "../constants/urls"
 const characterService = {
     getAll: () => apiService.get(urls.characters.base),
     getById: (id) => apiService.get(urls.characters.byId(id)),
-   getByEpisodeId: (episodeId) => apiService.get(urls.episodes.base)
-
-
     getByEpisodeId: async (episodeId) => {
         const response = await apiService.get(`${baseURL}/episode/${episodeId}`);
         const characterUrls = response.data.characters;
